2c4ad2fcc7fc9f349c2ffcdd53895470e3fe9ca2
bkalenderoglu <bkalenderoglu1@gmail.com>
2023-02-25 20:07:49 +0300

    math optimization

--- Assets/RadialTrigger.cs
+++ Assets/RadialTrigger.cs
@@ -5,20 +5,24 @@ using UnityEditor;
 public class RadialTrigger : MonoBehaviour
 {
     public float radius = 1f;
-    public Transform objTf;
+    public Transform player;
 
 
     #if UNITY_EDITOR
     void OnDrawGizmos(){
         Vector2 center = transform.position;
-        Vector2 objPos = objTf.position;
 
-        float distance = Vector2.Distance(center, objPos);
+        if(player == null) return;
+
+        Vector2 playerPos = player.position;
+        Vector2 delta = center - playerPos; // vector
+        float sqrDistance = delta.x * delta.x + delta.y * delta.y; // squared length of vector delta, another way -> Vector2.Dot(delta, delta)
 
 
-        bool isInside = distance <= radius;
+        //float distance = Vector2.Distance(center, playerPos);
+        bool isInside = sqrDistance <= radius * radius;
         Handles.color = isInside ? Color.green : Color.red;
-        
+
         //DrawWireDisc(Vector3 center, Vector3 normal, float radius)
         // normal vector specifies what orientation that disc should have
         Handles.DrawWireDisc(center, Vector3.forward, radius);
